f8bb33c5047108bfa19a070a9ed8c93256a0bd48
Jaroslav Tulach <jtulach@netbeans.org>
2012-06-12 09:17:43 +0200

    Geekout protocol registered via META-INF/services

---
+++ UniversalResourceLocator/src/org/netbeans/geekout/demo/impl/GeekoutProtocolFactory.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2012 Jaroslav Tulach <jtulach@netbeans.org>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.netbeans.geekout.demo.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+
+/**
+ *
+ * @author Jaroslav Tulach <jtulach@netbeans.org>
+ */
+public final class GeekoutProtocolFactory implements URLStreamHandlerFactory {
+    @Override
+    public URLStreamHandler createURLStreamHandler(String protocol) {
+        if ("geekout".equals(protocol)) {
+            return new GeekoutHandler();
+        }
+        return null;
+    }
+
+    private static class GeekoutHandler extends URLStreamHandler {
+
+        public GeekoutHandler() {
+        }
+
+        @Override
+        protected URLConnection openConnection(URL u) throws IOException {
+            return new GeekOutConnection(u);
+        }
+    }
+
+    private static class GeekOutConnection extends URLConnection {
+        private final ByteArrayInputStream is;
+        
+        public GeekOutConnection(URL url) throws UnsupportedEncodingException {
+            super(url);
+            String msg = "Hello " + url.getHost() + "!";
+            is = new ByteArrayInputStream(msg.getBytes("UTF-8"));
+        }
+
+        @Override
+        public void connect() throws IOException {
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            return is;
+        }
+        
+        
+    }
+}
